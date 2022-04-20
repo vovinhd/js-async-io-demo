c32f3da30f8dd734dffd09f2d4f899d82636a035
vovinhd <timo.volk@gmail.com>
2022-04-20 10:36:18 +0200

    eh

--- site/preload.js
+++ site/preload.js
@@ -1,9 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  startDoing: (seconds, tag) => ipcRenderer.send('start-doing', seconds, tag),
-  onMessage: (data) => ipcRenderer.on('message', data),
-  onClose: (tag) => ipcRenderer.on('close', tag)
 })
 
 
