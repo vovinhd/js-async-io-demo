56f6dd89ca996c8fe04511536bc369492ba50ffb
vovinhd <timo.volk@gmail.com>
2022-04-20 10:36:18 +0200

    eh

--- main.js
+++ main.js
@@ -1,26 +1,5 @@
 const path = require('path');
 const { app, BrowserWindow, ipcMain } = require('electron');
-const ChildProcess = require('child_process');
-
-const spawnThingDoer = async (seconds, tag, event) => {
-    const thingDoer = ChildProcess.spawn("node", ["thing-doer.js", seconds, tag]);
-    thingDoer.stdout.on('data', (data) => {
-        console.log(`${data}`);
-        event.reply('message', {
-            data,
-            tag
-        })
-    });
-    
-    thingDoer.on('close', () => {
-        event.reply('close', {
-            tag
-        })
-    })
-}
-
-// spawnThingDoer(4, "proc 1");
-// spawnThingDoer(4, "proc 2");
 
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -30,13 +9,6 @@ const createWindow = () => {
             preload: path.join(__dirname, 'site/preload.js')
         }
     })
-
-    ipcMain.on('start-doing', (event, seconds, tag) => {
-        const webContents = event.sender;
-        const win = BrowserWindow.fromWebContents(webContents);
-        spawnThingDoer(seconds, tag, event);
-    });
-
 
     win.loadFile('site/index.html')
 }
